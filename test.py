c252439ce12e0ae04ed3b7875874a5b5b3fce4e4
eshiffer73 <eshiffer@purdue.edu>
2025-10-05 17:00:00 -0400

    Added description of what needs to be done for tasks. Next step is creating functions for each step.

---
+++ test.py
@@ -0,0 +1,29 @@
+# Create goals #
+
+# Create page to hold list of goals #
+
+# Create button to add new goal #
+
+# Create form to input goal details #
+
+# Create function to save goal to database #
+
+# Create function to display list of goals #
+
+# Create function to mark goal as completed #
+
+# Create function to delete goal #
+
+# Create function to edit goal details #
+
+# Create function to set reminders for goals #
+
+# Create function to categorize goals #
+
+# Create function to prioritize goals #
+
+# Create function to track progress of goals #
+
+
+
+
